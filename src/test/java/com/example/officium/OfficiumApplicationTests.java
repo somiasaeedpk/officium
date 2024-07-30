b70fc27987dd411086e14bfea94b42744f8ed51a
somia295 <somiasaeedpk@gmail.com>
2024-07-30 11:53:21 +0500

    spring boot project initialized

---
+++ src/test/java/com/example/officium/OfficiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.officium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
